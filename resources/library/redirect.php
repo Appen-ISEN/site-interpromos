849b3fc708c40131f5ca7ec219319fa1e302568c
sehnryr <youn@melois.dev>
2022-10-15 18:47:24 +0200

    fix: remove buggy scheme on localhost

--- resources/library/redirect.php
+++ resources/library/redirect.php
@@ -13,9 +13,8 @@
  */
 function redirect(string $page): void
 {
-    $scheme = $_SERVER['REQUEST_SCHEME'];
     $host  = $_SERVER['HTTP_HOST'];
     $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-    header("Location: $scheme://$host$uri/$page");
+    header("Location: //$host$uri/$page");
     exit;
 }
