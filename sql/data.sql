1bcd2412ab8751dd5d7de6b0c2615e6976a57f3a
sehnryr <youn@melois.dev>
2022-10-15 18:55:25 +0200

    hard code admin account

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,5 @@
 DELETE FROM sports;
+DELETE FROM users;
 
 ALTER SEQUENCE sports_id_seq RESTART;
 INSERT INTO sports ("name") values
@@ -7,3 +8,8 @@ INSERT INTO sports ("name") values
 ('Bad'),
 ('Volley'),
 ('Futsal');
+
+-- Populate users table
+ALTER SEQUENCE users_id_seq RESTART;
+INSERT INTO users ("name", "email", "password_hash") VALUES
+('Appen', 'appenisen@proton.me', '$2y$10$5xFOYw3o0c5Py.mNpOHSl.WiNdWRvxbNPLEhr.9/wZzo7.LL0OEl6');
