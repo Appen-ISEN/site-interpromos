eff2527d58f6cb3533a132be2ba89bd1db72fbfe
0nano <35236525+0nano@users.noreply.github.com>
2022-10-17 18:39:04 +0200

    correction direct

--- public_html/js/script_direct.js
+++ public_html/js/script_direct.js
@@ -1,6 +1,6 @@
 function parseISOLocal(s) {
     var b = s.split(/\D/);
-    return new Date(b[0], b[1]+2, b[2], b[3], b[4], b[5]);
+    return new Date(b[0], b[1], b[2], b[3]+2, b[4], b[5]);
 }
 
 function getDirect() {
