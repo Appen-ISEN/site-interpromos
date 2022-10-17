390abb3cf0f0300cbbdd0ed28afe9801c6719dea
0nano <35236525+0nano@users.noreply.github.com>
2022-10-17 20:17:32 +0200

    adjust with delay

--- public_html/js/script_direct.js
+++ public_html/js/script_direct.js
@@ -19,7 +19,7 @@ function aspect(matchs) {
     matchs.forEach(match => {
         let date = new Date(match['date']);
         //show next matches with a gap of 20 min
-        if(date > Date.now() /*&& date < (Date.now() + 1200000)*/){
+        if(date > (Date.now() - 1200000) /*&& date < (Date.now() + 1200000)*/){
             let tname = JSON.parse(match['teams_name'])
 
             let cap = "<div class=\"capsule\">"+
@@ -32,7 +32,7 @@ function aspect(matchs) {
             $("#nextMatch").append(cap);
         }
         //show direct match with a gap of 20 min 
-        if(date < Date.now() && date > (Date.now() - 1800000)){
+        if(date < (Date.now() - 1200000) && date > ((Date.now() - 1200000) - 1800000)){
             let tname = JSON.parse(match['teams_name'])
             let tscore = JSON.parse(match['scores'])
 
